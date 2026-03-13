9e90fd7fce66521c73e0ff5493a9fa4dba08b342
Saikat Bala <saikat.bala25@gmail.com>
2026-03-13 21:26:36 +0530

    remove the unuse Autowired

--- src/main/java/com/example/JWTj21sb320/config/SecurityConfig.java
+++ src/main/java/com/example/JWTj21sb320/config/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.example.JWTj21sb320.config;
 
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
